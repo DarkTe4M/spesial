373006d37556ea8fe096bcac6129249061cff671
saputracharlezdavids852 <56171845+saputracharlezdavids852@users.noreply.github.com>
2019-10-10 21:52:40 +0700

    Add files via upload

---
+++ charlezyt.sh
@@ -0,0 +1,142 @@
+#!/system/xbin/bash
+# Bersihkan Layar
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+###################################################
+# Channel CHARLEz YT
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo  $red"[#]> (Ctrl + C ) Detected, 
+Trying To Exit ... "
+sleep 1
+echo ""
+echo  $yellow"[#]> Thank You For Using My 
+Tools ... "
+sleep 1
+echo ""
+echo  $white"[#]> Dark Cyber
+Here ... "
+read enter
+exit
+}
+cowsay -f dragon "Mr.CDS16" | lolcat
+figlet DARK CYBER | lolcat
+echo "╔==================================================================╗" | lolcat
+echo "║ [ • ] DARK CYBER                                                 ║" | lolcat
+echo "║ [ • ] NO WA : 087888023633                            ║" | lolcat
+echo "║ [ • ] THANKS TO :CHARLEz YT                                       ║" | lolcat
+echo "║ [ • ] ::::YOUTUBE CHANNEL::::                                    ║" | lolcat
+echo "║ [ • ] https://www.youtube.com/channel/UCOAAa4uhKPdwantiHLD2TIA ║" | lolcat
+echo "╚==================================================================╝" | lolcat
+echo "╔═══════════════════════════════════════════╗" | lolcat
+echo "║ .:::::::::::::::::TE4M::::::::::::::::::. ║" | lolcat
+echo "║ .::::::::::::::DARK  CYBER::::::::::::::. ║" | lolcat
+echo "╚═══════════════════════════════════════════╝" | lolcat
+sleep 1
+echo  $yellow" 01) VIRTEXT [DC] DARK CYBER"
+echo  $yellow" 02) SPAM SMS BY zlucifer"
+echo  $yellow" 03) VIRTEXT BY  ⃘⃤ᴋɪʀᴀꙮ≈⊰0⊱"
+echo  $yellow" 04) DOWNLOAD APK SMS GRATIS"
+echo  $yellow" 00) CEK IP"
+echo  $green""
+read -p "[CHARLEz YT]> " skuy;
+
+if [ $bro = 1 ] || [ $bro = 01 ]
+then
+clear
+echo  $red" CREATOR : CHARLEz YT"
+sleep 1
+echo  $red" MOHON TUNGGU SEBENTAR "
+sleep 3
+echo  $red"loading......"
+sleep 2
+echo  $red"ANDA AKAN DI ARAHKAN KE LINK DOWNLOAD NYA"
+sleep 5
+xdg-open 'https://realsht.mobi/TffyO'
+fi
+
+if [ $bro = 2 ] || [ $bro = 02 ]
+then
+clear
+echo  $red" SPAM SMS BY zlucifer "
+sleep 1
+echo  $red" ingat script ini bukan milik saya "
+sleep 1
+echo  $red" loading..... "
+sleep 3
+pkg update && pkg upgrade
+pkg install git
+pkg install curl
+git clone https://github.com/zlucifer/troll_project
+cd troll_project
+chmod +×*
+ls
+bash troll.sh
+clear
+fi
+
+if [ $bro = 3 ] || [ $bro = 03 ]
+then
+clear
+echo  $red"MOHON TUNGGU SEBENTAR"
+sleep 3
+echo  $red"ANDA AKAN DI ARAHKAN KE LINK DOWNLOAD"
+sleep 5
+xdg-open 'https://realsht.mobi/vbUzG'
+
+sleep 5
+clear
+exit
+fi
+
+if [ $bro = 4 ] || [ $bro = 04 ]
+then
+clear
+echo  $green"SUBSCRIBE DLU CHANNEL CHARLEz YT"
+sleep 1
+echo  $green"GUE KASIH WAKTU 10 DETIK"
+xdg-open 'https://www.youtube.com/channel/UCOAAa4uhKPdwantiHLD2TIA'
+sleep 10
+echo  $red"MOHON TUNGGU SEBENTAR"
+sleep 3
+echo  $red"ANDA AKAN DI ARAHKAN KE LINK DOWNLOAD"
+sleep 1
+echo  $red"Loading......."
+sleep 3
+xdg-open 'https://realsht.mobi/ZHxrg'
+sleep 5
+clear
+exit
+fi
+
+
+
+
+if [ $bro = 0 ] || [ $bro = 00 ]
+then
+clear
+echo  $red" CEK IP "
+sleep 1
+echo  $red"TUNGGU SEBENTAR"
+sleep 1
+echo  $red"SUBSCRIBE CHANNEL CHARLEz YT"
+sleep 3
+xdg-open 'https://www.youtube.com/channel/UCOAAa4uhKPdwantiHLD2TIA'
+pkg update
+pkg upgrade
+pkg install net-tools
+ifconfig
+
+sleep 3
+
+exit
+fi
+
